9815c30b675e5fd1aaae4dc7dcabffa89f1e24fe
ys computer <ys computer@DESKTOP-PC759FE>
2021-08-04 22:07:02 +0900

    상영영화 및 상영시간

---
+++ Project/src/PlayingMovie.java
@@ -0,0 +1,169 @@
+
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PlayingMovie extends JPanel {
+	
+	private MainFrame mf;
+	private JPanel PlayingMovie;
+	
+	public PlayingMovie(MainFrame mf) {
+		this.mf = mf;
+		this.PlayingMovie = this;
+		
+		this.setLayout(null);
+		this.setBackground(Color.white);
+		
+		JLabel label = new JLabel();
+		label.setText("상영 영화");
+		label.setFont(label.getFont().deriveFont(30.0f));  // 글씨크기
+		label.setBounds(320, 8, 200, 50);
+		this.add(label);
+		
+		// 이전버튼
+		JButton backBt = new JButton("이  전");
+		backBt.setBounds(60, 10, 80, 40);
+		backBt.setBackground(Color.white);
+		backBt.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		this.add(backBt);
+		
+		// 영화 이미지
+		Image i1 = new ImageIcon("movie/blackwidow.png").getImage().getScaledInstance(110, 150, Image.SCALE_SMOOTH);
+		Image i2 = new ImageIcon("movie/mogadishu.png").getImage().getScaledInstance(110, 150, Image.SCALE_SMOOTH);
+		Image i3 = new ImageIcon("movie/phoenix.png").getImage().getScaledInstance(110, 150, Image.SCALE_SMOOTH);
+		Image i4 = new ImageIcon("movie/rangjong.png").getImage().getScaledInstance(110, 150, Image.SCALE_SMOOTH);
+		
+		JLabel mImage1 = new JLabel(new ImageIcon(i1));
+		JLabel mImage2 = new JLabel(new ImageIcon(i2));
+		JLabel mImage3 = new JLabel(new ImageIcon(i3));
+		JLabel mImage4 = new JLabel(new ImageIcon(i4));
+		
+		mImage1.setBounds(60, 90, 120, 160);
+		mImage2.setBounds(240, 90, 120, 160);
+		mImage3.setBounds(420, 90, 120, 160);
+		mImage4.setBounds(600, 90, 120, 160);
+		
+		this.add(mImage1);
+		this.add(mImage2);
+		this.add(mImage3);
+		this.add(mImage4);
+		// 블랙위도우
+		JLabel mTitle1 = new JLabel("제목 : 블랙 위도우 (134분)", JLabel.CENTER);
+		JLabel mDirector1 = new JLabel("감독 : 케이트 쇼트랜드", JLabel.CENTER);
+		JLabel mGrade1 = new JLabel("평점 : 9.08", JLabel.CENTER);
+		// 모가디슈
+		JLabel mTitle2 = new JLabel("제목 : 모가디슈 (121분)", JLabel.CENTER);
+		JLabel mDirector2 = new JLabel("감독 : 류승완", JLabel.CENTER);
+		JLabel mGrade2 = new JLabel("평점 : 9.16", JLabel.CENTER);
+		// 피닉스
+		JLabel mTitle3 = new JLabel("제목 : 피닉스 (98분)", JLabel.CENTER);
+		JLabel mDirector3 = new JLabel("감독 : 크리스티안 펫졸드", JLabel.CENTER);
+		JLabel mGrade3 = new JLabel("평점 : 8.43", JLabel.CENTER);
+		// 랑종
+		JLabel mTitle4 = new JLabel("제목 : 랑종 (131분)", JLabel.CENTER);
+		JLabel mDirector4 = new JLabel("감독 : 반종 피산다나쿤", JLabel.CENTER);
+		JLabel mGrade4 = new JLabel("평점 : 6.82", JLabel.CENTER);
+		
+		mTitle1.setBounds(16, 260, 200, 20);
+		mDirector1.setBounds(16, 300, 200, 20);
+		mGrade1.setBounds(16, 340, 200, 20);
+		mTitle2.setBounds(198, 260, 200, 20);
+		mDirector2.setBounds(198, 300, 200, 20);
+		mGrade2.setBounds(198, 340, 200, 20);
+		mTitle3.setBounds(380, 260, 200, 20);
+		mDirector3.setBounds(380, 300, 200, 20);
+		mGrade3.setBounds(380, 340, 200, 20);
+		mTitle4.setBounds(560, 260, 200, 20);
+		mDirector4.setBounds(560, 300, 200, 20);
+		mGrade4.setBounds(560, 340, 200, 20);
+		// 패널 올리기
+		this.add(mTitle1);
+		this.add(mDirector1);
+		this.add(mGrade1);
+		this.add(mTitle2);
+		this.add(mDirector2);
+		this.add(mGrade2);
+		this.add(mTitle3);
+		this.add(mDirector3);
+		this.add(mGrade3);
+		this.add(mTitle4);
+		this.add(mDirector4);
+		this.add(mGrade4);
+		
+		// 예매버튼
+		JButton mButton1 = new JButton("영화 예매");
+		JButton mButton2 = new JButton("영화 예매");
+		JButton mButton3 = new JButton("영화 예매");
+		JButton mButton4 = new JButton("영화 예매");
+	
+		mButton1.setBounds(70, 380, 100, 40);
+		mButton2.setBounds(250, 380, 100, 40);
+		mButton3.setBounds(430, 380, 100, 40);
+		mButton4.setBounds(610, 380, 100, 40);
+		// 버튼 배경색
+		mButton1.setBackground(new Color(33, 150, 83));
+		mButton2.setBackground(new Color(33, 150, 83));
+		mButton3.setBackground(new Color(33, 150, 83));
+		mButton4.setBackground(new Color(33, 150, 83));
+		// 버튼 테두리색
+		mButton1.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		mButton2.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		mButton3.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		mButton4.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		// 버튼 글자색
+		mButton1.setForeground(Color.white);
+		mButton2.setForeground(Color.white);
+		mButton3.setForeground(Color.white);
+		mButton4.setForeground(Color.white);
+		// 패널 올리기
+		this.add(mButton1);
+		this.add(mButton2);
+		this.add(mButton3);
+		this.add(mButton4);
+		
+		// 예매버튼 클릭시
+		mButton1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new BwTime(mf);
+				setVisible(false);
+			}
+		});
+		mButton2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new MgTime(mf);
+				setVisible(false);
+			}
+		});
+		mButton3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new PnTime(mf);
+				setVisible(false);
+			}
+		});
+		mButton4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new RjTime(mf);
+				setVisible(false);
+			}
+		});
+		
+		// 메인프레임에 패널 추가
+		mf.add(this);
+	}
+}
---
+++ Project/src/PnTime.java
@@ -0,0 +1,271 @@
+
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class PnTime extends JPanel {
+
+	private MainFrame mf;
+	private JPanel BwTime;
+	
+	public PnTime(MainFrame mf) {  // <= 저는 이게 없으면 오류나더라구요ㅠㅠ
+		this.mf = mf;
+		this.BwTime = this;
+		
+		this.setBackground(Color.white);
+		this.setLayout(null);
+		
+		JLabel label = new JLabel();
+		label.setText("상영 시간");
+		label.setFont(label.getFont().deriveFont(30.0f));  // 글씨크기
+		label.setBounds(320, 8, 200, 50);
+		this.add(label);
+		
+		// 이전버튼
+		JButton backbt = new JButton("이  전");
+		backbt.setBounds(60, 10, 80, 40);
+		backbt.setBackground(Color.white);
+		backbt.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		this.add(backbt);
+		// 이전버튼 클릭시 상영영화 페이지
+		backbt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new PlayingMovie(mf);
+				setVisible(false);
+			}
+		});
+		
+		// 영화이미지
+		Image i1 = new ImageIcon("movie/phoenix.png").getImage().getScaledInstance(200, 250, Image.SCALE_SMOOTH);
+		JLabel mImage1 = new JLabel(new ImageIcon(i1));
+		mImage1.setBounds(60, 90, 200, 250);
+		this.add(mImage1);
+		// 제목
+		JLabel mTitle1 = new JLabel("피닉스 (98분)", JLabel.CENTER);
+		mTitle1.setFont(mTitle1.getFont().deriveFont(15.0f)); 
+		mTitle1.setBounds(60, 370, 200, 20);
+		this.add(mTitle1);
+		
+		// 라디오버튼
+		JRadioButton rButton1 = new JRadioButton();
+		rButton1.setBackground(Color.white);
+		JRadioButton rButton2 = new JRadioButton();
+		rButton2.setBackground(Color.white);
+		JRadioButton rButton3 = new JRadioButton();
+		rButton3.setBackground(Color.white);
+		JRadioButton rButton4 = new JRadioButton();
+		rButton4.setBackground(Color.white);
+		JRadioButton rButton5 = new JRadioButton();
+		rButton5.setBackground(Color.white);
+		JRadioButton rButton6 = new JRadioButton();
+		rButton6.setBackground(Color.white);
+		JRadioButton rButton7 = new JRadioButton();
+		rButton7.setBackground(Color.white);
+		JRadioButton rButton8 = new JRadioButton();
+		rButton8.setBackground(Color.white);
+		JRadioButton rButton9 = new JRadioButton();
+		rButton9.setBackground(Color.white);
+		
+		// 시간
+		JLabel time1 = new JLabel("06:00", JLabel.CENTER);
+		time1.setOpaque(true);
+		time1.setBackground(new Color(33, 150, 83));
+		time1.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time1.setFont(time1.getFont().deriveFont(15.0f));
+		time1.setForeground(Color.white);
+		JLabel time2 = new JLabel("08:30", JLabel.CENTER);
+		time2.setOpaque(true);
+		time2.setBackground(new Color(33, 150, 83));
+		time2.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time2.setFont(time2.getFont().deriveFont(15.0f));
+		time2.setForeground(Color.white);
+		JLabel time3 = new JLabel("11:00", JLabel.CENTER);
+		time3.setOpaque(true);
+		time3.setBackground(new Color(33, 150, 83));
+		time3.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time3.setFont(time3.getFont().deriveFont(15.0f));
+		time3.setForeground(Color.white);
+		JLabel time4 = new JLabel("13:30", JLabel.CENTER);
+		time4.setOpaque(true);
+		time4.setBackground(new Color(33, 150, 83));
+		time4.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time4.setFont(time4.getFont().deriveFont(15.0f));
+		time4.setForeground(Color.white);
+		JLabel time5 = new JLabel("16:00", JLabel.CENTER);
+		time5.setOpaque(true);
+		time5.setBackground(new Color(33, 150, 83));
+		time5.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time5.setFont(time5.getFont().deriveFont(15.0f));
+		time5.setForeground(Color.white);
+		JLabel time6 = new JLabel("18:30", JLabel.CENTER);
+		time6.setOpaque(true);
+		time6.setBackground(new Color(33, 150, 83));
+		time6.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time6.setFont(time6.getFont().deriveFont(15.0f));
+		time6.setForeground(Color.white);
+		JLabel time7 = new JLabel("21:00", JLabel.CENTER);
+		time7.setOpaque(true);
+		time7.setBackground(new Color(33, 150, 83));
+		time7.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time7.setFont(time7.getFont().deriveFont(15.0f));
+		time7.setForeground(Color.white);
+		JLabel time8 = new JLabel("23:30", JLabel.CENTER);
+		time8.setOpaque(true);
+		time8.setBackground(new Color(33, 150, 83));
+		time8.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time8.setFont(time8.getFont().deriveFont(15.0f));
+		time8.setForeground(Color.white);
+		JLabel time9 = new JLabel("26:00", JLabel.CENTER);
+		time9.setOpaque(true);
+		time9.setBackground(new Color(33, 150, 83));
+		time9.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		time9.setFont(time9.getFont().deriveFont(15.0f));
+		time9.setForeground(Color.white);	
+		
+		// 위치 및 크기
+		rButton1.setBounds(300, 100, 25, 25);		
+		time1.setBounds(330, 90, 80, 50);
+		rButton2.setBounds(450, 100, 25, 25);		
+		time2.setBounds(480, 90, 80, 50);
+		rButton3.setBounds(600, 100, 25, 25);		
+		time3.setBounds(630, 90, 80, 50);
+		rButton4.setBounds(300, 170, 25, 25);		
+		time4.setBounds(330, 160, 80, 50);
+		rButton5.setBounds(450, 170, 25, 25);		
+		time5.setBounds(480, 160, 80, 50);
+		rButton6.setBounds(600, 170, 25, 25);		
+		time6.setBounds(630, 160, 80, 50);
+		rButton7.setBounds(300, 240, 25, 25);		
+		time7.setBounds(330, 230, 80, 50);
+		rButton8.setBounds(450, 240, 25, 25);		
+		time8.setBounds(480, 230, 80, 50);
+		rButton9.setBounds(600, 240, 25, 25);		
+		time9.setBounds(630, 230, 80, 50);	
+		// 라디오버튼 그룹화
+		ButtonGroup rbg = new ButtonGroup();
+		rbg.add(rButton1);
+		rbg.add(rButton2);
+		rbg.add(rButton3);
+		rbg.add(rButton4);
+		rbg.add(rButton5);
+		rbg.add(rButton6);
+		rbg.add(rButton7);
+		rbg.add(rButton8);
+		rbg.add(rButton9);
+		// 패널에 올리기
+		this.add(time1);
+		this.add(time2);
+		this.add(time3);
+		this.add(time4);
+		this.add(time5);
+		this.add(time6);
+		this.add(time7);
+		this.add(time8);
+		this.add(time9);		
+		this.add(rButton1);
+		this.add(rButton2);
+		this.add(rButton3);
+		this.add(rButton4);
+		this.add(rButton5);
+		this.add(rButton6);
+		this.add(rButton7);
+		this.add(rButton8);
+		this.add(rButton9);
+		
+		// 확인 문구
+		JLabel msg = new JLabel("시간 선택해 주십시오.", JLabel.CENTER);
+		msg.setBounds(370, 300, 300, 50);
+		msg.setFont(msg.getFont().deriveFont(15.0f));
+		this.add(msg);
+		// 확인 버튼
+		JButton checkbt = new JButton("확 인");
+		checkbt.setBounds(470, 360, 90, 40);
+		checkbt.setFont(checkbt.getFont().deriveFont(15.0f));
+		checkbt.setForeground(Color.white);
+		checkbt.setBackground(new Color(33, 150, 83));
+		checkbt.setBorder(BorderFactory.createLineBorder(new Color(33, 150, 83)));
+		this.add(checkbt);
+		// 이전버튼 클릭시 상영영화 페이지
+		checkbt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// 좌석 선택 화면으로~~~~~~~~~~~
+				
+				setVisible(false);
+			}
+		});
+		
+		// 시간 선택시 문구 출력 
+		rButton1.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time1.getText() + " 예매 맞습니까?");
+			}
+		});
+		rButton2.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time2.getText() + " 예매 맞습니까?");
+			}
+		});
+		rButton3.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time3.getText() + " 예매 맞습니까?");
+			}
+		});
+		rButton4.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time4.getText() + " 예매 맞습니까?");
+			}
+		});
+		rButton5.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time5.getText() + " 예매 맞습니까?");
+			}
+		});
+		rButton6.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time6.getText() + " 예매 맞습니까?");
+			}
+		});
+		rButton7.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time7.getText() + " 예매 맞습니까?");
+			}
+		});
+		rButton8.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time8.getText() + " 예매 맞습니까?");
+			}
+		});
+		rButton9.addMouseListener(new MouseAdapter() {
+			@Override  
+			public void mouseClicked(MouseEvent e) {  
+				msg.setText(mTitle1.getText()+ " " + time9.getText() + " 예매 맞습니까?");
+			}
+		});
+		
+		// 메인프레임에 패널 추가		
+		mf.add(this);
+	}	
+}
