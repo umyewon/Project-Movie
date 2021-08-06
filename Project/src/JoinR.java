5aa8ebaf075949c5dfe06364e3b7d1e450ff5ba0
82109 <82109@DESKTOP-DDHEBRN>
2021-08-06 20:47:41 +0900

    가입 창의 텍스트 필드 값 10 -> 13 으로 변경

--- Project/src/JoinR.java
+++ Project/src/JoinR.java
@@ -29,7 +29,6 @@ public class JoinR extends JPanel{
 		
 		// 패널
 		JoinR = new JPanel();
-		//setLayout(null);
 		setBackground(Color.white);	
 	
 		
@@ -43,50 +42,49 @@ public class JoinR extends JPanel{
 		
 		// 공백
 		JLabel tab1 = new JLabel();
-		tab1.setText("* * * * * * * * * * *");
+		tab1.setText("* * * * * * * * * * * * * *");
 		tab1.setFont(font1);
 		tab1.setForeground(new Color(33, 150, 83));
 		
 		// id
 		JLabel idL = new JLabel();
-		//idL.setHorizontalAlignment(JLabel.CENTER);
-		idL.setText("                   ID");
+		idL.setText("                        ID");
 		idL.setFont(font1);
 		idL.setForeground(new Color(33, 150, 83));
 		
-		idT = new JTextField(10);
+		idT = new JTextField(13);
 		idT.setFont(font2);	
 		
 		// pw
 		JLabel pwL = new JLabel();
-		pwL.setText("         Password");
+		pwL.setText("               Password");
 		pwL.setFont(font1);
 		pwL.setForeground(new Color(33, 150, 83));
 		
-		pwT = new JPasswordField(10);
+		pwT = new JPasswordField(13);
 		pwT.setFont(font2);	
 		
 		// name
 		JLabel nameL = new JLabel();
-		nameL.setText("              Name");
+		nameL.setText("                   Name");
 		nameL.setFont(font1);
 		nameL.setForeground(new Color(33, 150, 83));
 		
-		nameT = new JTextField(10);
+		nameT = new JTextField(13);
 		nameT.setFont(font2);		
 		
 		// tel
 		JLabel telL = new JLabel();
-		telL.setText("                 Tel");
+		telL.setText("                      Tel");
 		telL.setFont(font1);
 		telL.setForeground(new Color(33, 150, 83));
 		
-		telT = new JTextField(10);
+		telT = new JTextField(13);
 		telT.setFont(font2);	
 		
 		// 공백
 		JLabel tab2 = new JLabel();
-		tab2.setText("* * * * * * * * * * *");
+		tab2.setText("* * * * * * * * * * * * * *");
 		tab2.setFont(font1);
 		tab2.setForeground(new Color(33, 150, 83));
 		
@@ -127,7 +125,7 @@ public class JoinR extends JPanel{
 		panel.add(tab2);
 		panel.add(join);
 				
-		panel.setPreferredSize(new Dimension(200, 500));
+		panel.setPreferredSize(new Dimension(250, 500));
 		add(panel);
 		
 		mf.add(this);
